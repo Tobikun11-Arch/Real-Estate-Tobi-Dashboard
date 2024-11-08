beb206599fc687339be6760e23ffd81d4aa98cc2
Joenel <joenelsevellejo831@gmail.com>
2024-11-08 22:29:06 +0800

    npm error fixed

--- app/Components/ui/NavBar.tsx
+++ app/Components/ui/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const NavBar = () => {
@@ -6,7 +6,7 @@ const NavBar = () => {
 
     return (
         <nav className='h-12 flex items-center justify-between px-4'>
-            <h1 className='font-bold text-blue-700' onClick={()=> router.push('/')}>SEWO</h1>
+            <h1 className='font-bold text-blue-700 z-50' onClick={()=> router.push('/')}>SEWO</h1>
             <p className='text-black text-sm font-semibold'>Login</p>
         </nav>
     )
